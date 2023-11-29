d4b30b36d99b2120ce1011afeb9cc824cf9bc1ae
VaibhavKukade <vaibhavkukade2611@gmail.com>
2023-11-29 13:15:34 +0530

    first commit

---
+++ src/test/java/com/app/matrimonial/MatrimonialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.matrimonial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatrimonialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
