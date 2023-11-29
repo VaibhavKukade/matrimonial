ab90c2ed14e4399b08bb0f1e06c93e856481fa21
VaibhavKukade <vaibhavkukade2611@gmail.com>
2023-11-29 15:41:14 +0530

    user api implementation

--- src/main/java/com/app/matrimonial/MatrimonialApplication.java
+++ src/main/java/com/app/matrimonial/MatrimonialApplication.java
@@ -2,12 +2,18 @@ package com.app.matrimonial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-public class MatrimonialApplication {
+@Configuration
+@ImportResource
+public class MatrimonialApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MatrimonialApplication.class, args);
+		System.out.println("Application Started");
 	}
 
 }
