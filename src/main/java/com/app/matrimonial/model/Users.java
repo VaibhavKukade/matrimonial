020333a7afae2fbd3ef71a868d489870a13ad681
VaibhavKukade <vaibhavkukade2611@gmail.com>
2023-11-29 14:39:11 +0530

    define the structure of project

---
+++ src/main/java/com/app/matrimonial/model/Users.java
@@ -0,0 +1,22 @@
+package com.app.matrimonial.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+}
