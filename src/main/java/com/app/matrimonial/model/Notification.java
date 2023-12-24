e55023f6bd46dfb372a620043598cbbf8aa10ba0
Abhay Shukla <abhay.zengatv@gmail.com>
2023-12-24 20:54:50 +0530

    "Notifiaction...."

--- src/main/java/com/app/matrimonial/model/Notification.java
+++ src/main/java/com/app/matrimonial/model/Notification.java
@@ -2,12 +2,16 @@ package com.app.matrimonial.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "notification")
 public class Notification {
